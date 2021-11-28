d1ac194280e4242fa4ee58d9c37379268cff7a21
Khalil <khalilmeziou@yahoo.fr>
2021-11-28 15:54:00 +0100

    adding Items

--- app/src/main/java/com/nicoalex/todo/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/nicoalex/todo/tasklist/TaskListFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.nicoalex.todo.R
+import java.util.*
 
 class TaskListFragment : Fragment(){
-    private val taskList = listOf(
+    private val taskList = mutableListOf(
         Task(id = "id_1", title = "Task 1", description = "description 1"),
         Task(id = "id_2", title = "Task 2"),
         Task(id = "id_3", title = "Task 3")
@@ -20,6 +24,10 @@ class TaskListFragment : Fragment(){
         val recyclerView  = view.findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(activity)
         recyclerView.adapter = TaskListAdapter(taskList)
+        view.findViewById<FloatingActionButton>(R.id.AddingTaskButton).setOnClickListener{
+            taskList.add(Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}"))
+            recyclerView.adapter = TaskListAdapter(taskList)
+        }
     }
     override fun onCreateView(
         inflater: LayoutInflater,
